9f54abb2f86184e2378bef5f9af88ba3120e0b07
Pawan <nepsonh@gmail.com>
2022-05-06 13:12:09 +0545

    Some important css added with header component

--- src/components/experience/Expericence.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import "./expericence.css"
-export const Expericence = () => {
-    return (
-        <div>Expericence</div>
-    )
-}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import "./header.css"
+import { Quote } from './Quote'
 export const Header = () => {
     return (
-        <div>Header</div>
+        <header>
+            <div className="container header__container">
+                <h5>Helllo I'm</h5>
+                <h1>Pawan Upadhayay (NEPSON)</h1>
+                <h5 className='text-light'>Front-End Developer</h5>
+                <Quote />
+            </div>
+        </header>
     )
 }
