cb8ba54515beeb78c63669e42d3b7b38fb71ebee
Pawan <nepsonh@gmail.com>
2022-06-16 09:40:18 +0545

    usestate hook is used to keep track of active states for various components

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,23 +1,45 @@
+import { useState } from "react";
 import { FaBook, FaHome, FaUser } from "react-icons/fa";
 import { MdDesignServices, MdMessage } from "react-icons/md";
 import "./nav.css";
 export const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         {" "}
         <FaHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <FaUser />{" "}
       </a>
-      <a href="#experience">
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
         <FaBook />{" "}
       </a>
-      <a href="#services">
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
         <MdDesignServices />{" "}
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <MdMessage />{" "}
       </a>
     </nav>
