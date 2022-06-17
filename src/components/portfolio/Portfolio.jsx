02add938f86ceb37d06689a34be2a09813a6ddc2
Pawan <nepsonh@gmail.com>
2022-06-17 23:34:18 +0545

    portfolio section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,131 @@
-import React from "react";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 import "./portfolio.css";
 export const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Current Work</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="Loading" />
+          </div>
+          <h3>Porfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="Loading" />
+          </div>
+          <h3>Porfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="Loading" />
+          </div>
+          <h3>Porfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="Loading" />
+          </div>
+          <h3>Porfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="Loading" />
+          </div>
+          <h3>Porfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="Loading" />
+          </div>
+          <h3>Porfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn">
+              Github
+            </a>
+
+            <a
+              href="https://github.com"
+              className="btn btn-primary"
+              target="__blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
