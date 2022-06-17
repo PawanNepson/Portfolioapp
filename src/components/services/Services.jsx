ec92dde89e6d4aa737580075a7ec635549fc2961
Pawan <nepsonh@gmail.com>
2022-06-17 15:01:09 +0545

    The service section done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,5 +1,117 @@
-import React from "react";
+import { MdCheck } from "react-icons/md";
 import "./services.css";
 export const Services = () => {
-  return <section id="services">Services</section>;
+  return (
+    <section id="services">
+      <h5>What I Offer To You</h5>
+      <h2>Services</h2>
+      <div className=" services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am Good at this but will later fill this thing</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+          </ul>
+        </article>
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+          <ul className="service__list">
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>One day this will be my new hobby.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+            <li>
+              <MdCheck className="service__list-icon" />
+              <p>I am learning figma and will be expert one day.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
