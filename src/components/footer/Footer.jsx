3cc1f3ae6239b8384f2e2e01e3059aef8fad20d3
Pawan <nepsonh@gmail.com>
2022-06-19 22:48:33 +0545

    footer section done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
-import "./footer.css"
+import { FaInstagram, FaTwitter } from "react-icons/fa";
+import { MdFacebook } from "react-icons/md";
+import "./footer.css";
 
 export const Footer = () => {
-    return (
-        <div>Footer</div>
-    )
-}
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Pawan
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <MdFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Pawan Upadhayay[Nepson] 2022.All r rights reserved</small>
+      </div>
+    </footer>
+  );
+};
