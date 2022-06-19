55295655c4a10d032b880a91b775f0f963853c70
Pawan <nepsonh@gmail.com>
2022-06-19 11:27:56 +0545

    swiper js used to make slide

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,12 @@ import AVTR1 from "../../assets/avatar1.jpg";
 import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
+import { Pagination } from "swiper";
 import "./testimonials.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
 const data = [
   {
     id: 1,
@@ -38,19 +43,25 @@ export const Testimonials = () => {
     <section id="testimonials">
       <h5>Recommendations From Seniors</h5>
       <h2>Recommendations</h2>
-      <div className="container testimonials__container">
+      <Swiper
+        className="container testimonials__container" // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
         {data.map(({ id, name, review, avatar }) => {
           return (
-            <article key={id} className="testimonial">
+            <SwiperSlide key={id} className="testimonial">
               <div className="client__avatar">
                 <img src={avatar} alt={name} />
               </div>
               <h5 className="client__name">{name}</h5>
               <small className="client__review">{review}</small>
-            </article>
+            </SwiperSlide>
           );
         })}
-      </div>
+      </Swiper>
     </section>
   );
 };
