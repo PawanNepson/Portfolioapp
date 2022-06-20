60887e360c9a853202b2a67e414b4d85b0206f6f
Pawan <nepsonh@gmail.com>
2022-06-20 15:09:19 +0545

    about section done

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,6 @@
 import { BsAward } from "react-icons/bs";
-import { MdWork, MdBuild } from "react-icons/md";
-import "./about.css";
+import { MdBuild, MdWork } from "react-icons/md";
+import styles from "./About.module.css";
 
 import ME from "../../assets/me-about.jpg";
 export const About = () => {
@@ -8,26 +8,26 @@ export const About = () => {
     <section id="about">
       <h5>You Can Explore</h5>
       <h2>About Me</h2>
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
+      <div className={`${styles.about__container} container`}>
+        <div className={styles.about__me}>
+          <div className={styles.about__me__image}>
             <img src={ME} alt="about  me image is loading" />
           </div>
         </div>
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <BsAward className="about__icon" />
+        <div className={styles.about__content}>
+          <div className={styles.about__cards}>
+            <article className={styles.about__card}>
+              <BsAward className={styles.about__icon} />
               <h5>Experince</h5>
               <small>6 months</small>
             </article>
-            <article className="about__card">
-              <MdWork className="about__icon" />
+            <article className={styles.about__card}>
+              <MdWork className={styles.about__icon} />
               <h5>Skills</h5>
               <small>CSS,React,Next Js</small>
             </article>
-            <article className="about__card">
-              <MdBuild className="about__icon" />
+            <article className={styles.about__card}>
+              <MdBuild className={styles.about__icon} />
               <h5>Projects</h5>
               <small>5+ projects</small>
             </article>
