8196d1d6681973cb5ebef1c10056048074ec5dc7
Pawan <nepsonh@gmail.com>
2022-06-20 15:51:02 +0545

    Portfolio section done

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import IMG3 from "../../assets/portfolio3.jpg";
 import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.png";
 import IMG6 from "../../assets/portfolio6.jpg";
-import "./portfolio.css";
+import styles from "./Portfolio.module.css";
 
 const data = [
   {
@@ -55,15 +55,15 @@ export const Portfolio = () => {
     <section id="portfolio">
       <h5>My Current Work</h5>
       <h2>Portfolio</h2>
-      <div className="container portfolio__container">
+      <div className={`${styles.portfolio__container} container`}>
         {data.map(({ id, image, title, github, demo }) => {
           return (
-            <article className="portfolio__item" key={id}>
-              <div className="portfolio__item-image">
+            <article className={styles.portfolio__item} key={id}>
+              <div className={styles.portfolio__item__image}>
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
-              <div className="portfolio__item-anc">
+              <div className={styles.portfolio__item__anc}>
                 <a href={github} className="btn">
                   Github
                 </a>
