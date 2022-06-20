2b1492c4567ffe8e4511a1ca09687799c2bff522
Pawan <nepsonh@gmail.com>
2022-06-20 15:44:58 +0545

    service section done

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,112 +1,112 @@
 import { MdCheck } from "react-icons/md";
-import "./services.css";
+import styles from "./Services.module.css";
 export const Services = () => {
   return (
     <section id="services">
       <h5>What I Offer To You</h5>
       <h2>Services</h2>
-      <div className=" services__container">
-        <article className="service">
-          <div className="service__head">
+      <div className={`${styles.services__container} container`}>
+        <article className={styles.service}>
+          <div className={styles.service__head}>
             <h3>UI/UX Design</h3>
           </div>
-          <ul className="service__list">
+          <ul className={styles.service__list}>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
           </ul>
         </article>
-        <article className="service">
-          <div className="service__head">
+        <article className={styles.service}>
+          <div className={styles.service__head}>
             <h3>Web Development</h3>
           </div>
-          <ul className="service__list">
+          <ul className={styles.service__list}>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am Good at this but will later fill this thing</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
           </ul>
         </article>
-        <article className="service">
-          <div className="service__head">
+        <article className={styles.service}>
+          <div className={styles.service__head}>
             <h3>Content Creation</h3>
           </div>
-          <ul className="service__list">
+          <ul className={styles.service__list}>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>One day this will be my new hobby.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
             <li>
-              <MdCheck className="service__list-icon" />
+              <MdCheck className={styles.service__list__icon} />
               <p>I am learning figma and will be expert one day.</p>
             </li>
           </ul>
